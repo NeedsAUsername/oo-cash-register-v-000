240a90a4b978d0c47999b90f2ba4b2327ecb0d64
Kevin Chi <kevinchi118@gmail.com>
2018-06-20 18:10:02 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
         if self.discount == nil
             puts "There is no discount to apply."
         else self.total *= ((100 - self.discount)/100.0)
-            puts "After the discount, the total comes to $#{new_total.to_i}."
+            puts "After the discount, the total comes to $#{self.total.to_i}."
         end
     end
 
